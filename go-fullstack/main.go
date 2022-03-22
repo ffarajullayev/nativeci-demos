b1726a078fa219972124027f6c13aefd57aef639
ffarajullayev <78479560+ffarajullayev@users.noreply.github.com>
2022-03-22 11:36:27 +0400

    Update main.go

--- go-fullstack/main.go
+++ go-fullstack/main.go
@@ -24,7 +24,7 @@ func main() {
 		})
 	})
 	r.GET("/loaderio-345f0e73092256270286441af8d5e2fe", func(c *gin.Context){
-		c.Data(200, "text/html; charset=utf-8", "loaderio-345f0e73092256270286441af8d5e2fe")
+		c.Data(200, "text/html; charset=utf-8", []byte("loaderio-345f0e73092256270286441af8d5e2fe"))
 	})
 	r.Run("0.0.0.0:8000")
 }
