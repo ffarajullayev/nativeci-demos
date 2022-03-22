624f6b18785cc0b31d9129dc3e4c7bef2c152c88
ffarajullayev <78479560+ffarajullayev@users.noreply.github.com>
2022-03-22 11:33:36 +0400

    Update main.go

--- go-fullstack/main.go
+++ go-fullstack/main.go
@@ -23,5 +23,9 @@ func main() {
 			"stories": stories,
 		})
 	})
+	r.GET("/loaderio-345f0e73092256270286441af8d5e2fe", func(c *gin.Context){
+		c.Header("Content-Type", "text/plain; charset=utf-8")
+		c.String("loaderio-345f0e73092256270286441af8d5e2fe")	
+	})
 	r.Run("0.0.0.0:8000")
 }
